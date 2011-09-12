4efdff0726cb47a48421601a734e9451ffd13cce
Adam <adamloving@gmail.com>
2011-09-11 22:08:49 -0700

    Convert results to array.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     @entries = Entry.limit(20).order('id desc')
     
     # todo: where created_at > 30 days ago
-    @rollups = ActiveRecord::Base.connection.execute(      'select sum(count) as count, unit, verb, noun        from entries        group by unit, verb, noun       order by count desc;')
-    @rollups.to_a.each { |r| r.symbolize_keys! }
+    @rollups = ActiveRecord::Base.connection.execute(      'select sum(count) as count, unit, verb, noun        from entries        group by unit, verb, noun       order by count desc;').to_a
+    @rollups.each { |r| r.symbolize_keys! }
   end
 end
