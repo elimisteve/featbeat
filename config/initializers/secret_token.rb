80f2803b745a408a487b3ca9335d692e4ff00beb
Adam <adamloving@gmail.com>
2011-09-11 19:58:29 -0700

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Featbeat::Application.config.secret_token = 'caf38fee401d88fb0aa8582f34726f7c5a8ed11aa5c3fc79871458cdd80b712a78324de48874d164d7fcc0b0368a27f88957643d380143726d502f6a62a95493'
