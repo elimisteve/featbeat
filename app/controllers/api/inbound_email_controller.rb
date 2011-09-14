a1332037978b3d3d63908fdab53163680fe9a654
Adam <adamloving@gmail.com>
2011-09-13 21:50:31 -0700

    create entry from inbound email

--- app/controllers/api/inbound_email_controller.rb
+++ app/controllers/api/inbound_email_controller.rb
@@ -2,6 +2,14 @@ class Api::InboundEmailController < ActionController::Base
   def create
     Rails.logger.info "got #{params}"
     
-    render :json => { :status => 'success' }
+    e = Entry.create_from_sentence(params[:subject])
+    
+    if e
+      result = { :status => 'success', :entry_id => e.id }
+    else
+      result = { :status => 'fail' }
+    end
+    
+    render :json => result
   end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,17 @@
 class Entry < ActiveRecord::Base
+  # "1 unit noun verbed"
+  def Entry.create_from_sentence(s)
+    a = s.split(' ')
+    
+    if a.size == 4
+      return Entry.create(
+        :count  => a[0],
+        :unit   => a[1],
+        :noun   => a[2],
+        :verb   => a[3]
+      )
+    else 
+      Rails.logger.warn "unrecognized sentence #{s}"
+    end
+  end
 end
