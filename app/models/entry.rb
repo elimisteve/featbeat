99d2e3dfbacde0b3b8a43e98ae392d7930882c18
Adam Loving <adamloving@gmail.com>
2011-10-23 16:04:39 -0700

    Handle phrases beginning with "I"

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,16 +1,35 @@
 class Entry < ActiveRecord::Base
-  # "1 unit noun verbed"
+  
   def Entry.create_from_sentence(s)
     a = s.split(' ')
+
     
-    if a.last == 'visited'
+    # I took 1 bus
+    # I ran 5 kilometers
+    # I ate 1 bannana
+    if a.first.upcase == 'I' 
+      
+      if a.size > 5 and a[4] == 'of' # I ate 7 grams of protein
+        noun = a[5]
+      else
+        noun = a[3]
+      end
+
+      return Entry.create(
+        :count  => a[2],
+        :unit   => a[3],
+        :noun   => noun,
+        :verb   => a[1]
+      )
+
+    elsif a.last == 'visited' # "1 venue something something something visited"
       return Entry.create(
         :count  => a[0],
         :unit   => a[1],
         :noun   => a[2..a.size-2].join(' '),
         :verb   => a.last
       )      
-    elsif a.size == 4
+    elsif a.size == 4 # "1 unit noun verbed"
       return Entry.create(
         :count  => a[0],
         :unit   => a[1],
