3ce14b65ef236ec57216636535a33cc5921d0c0a
Adam Loving <adamloving@gmail.com>
2013-01-27 17:55:37 -0800

    try a different rollup query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,12 @@ class HomeController < ApplicationController
     @entries = Entry.limit(20).order('id desc')
     
     # todo: where created_at > 30 days ago
-    @rollups = ActiveRecord::Base.connection.execute(      'select sum(count) as count, unit, verb, noun        from entries        group by unit, verb, noun       order by count desc;').to_a
+    @rollups = ActiveRecord::Base.connection.execute("""
+      select sum(count) as count, unit, verb, noun        
+      from entries group by unit, verb, noun
+      order by count desc
+      limit 20;""").to_a
+    
     @rollups.each { |r| r.symbolize_keys! }
   end
 end
