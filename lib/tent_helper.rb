e806bd12cea071aee0a835f74a0e9b89902742a8
Adam Loving <adamloving@gmail.com>
2013-02-13 18:35:12 -0800

    tent helper

---
+++ lib/tent_helper.rb
@@ -0,0 +1,21 @@
+class TentHelper
+
+  def self.post(user, text)
+    post = { 
+      permissions: {
+        public: true,
+        entities: {}
+      }, 
+        type: 'https://tent.io/types/post/status/v0.1.0',
+        content: {
+          text: text
+        }
+    }
+    
+    discovery = TentClient.new.discover(User.last.entity)
+    url = discovery.profile_urls.first.gsub(/profile/, 'posts')
+    client = TentClient.new(user.entity, :mac_key_id => user.mac_key_id, :mac_key => user.mac_key, :mac_algorithm => user.mac_algorithm)
+    client.post.create(post, :url => url) 
+  end
+
+end
