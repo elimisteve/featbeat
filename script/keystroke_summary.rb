90c3d794f46f1b4b67519f60e17096bfa3e8b0a9
Adam Loving <adamloving@gmail.com>
2013-02-24 09:05:33 -0800

    post keystrokes to featbeat

--- script/keystroke_summary.rb
+++ script/keystroke_summary.rb
@@ -1,19 +1,22 @@
 #!/usr/bin/env ruby
 
 # This script reads a plain text key logger file, and calculates
-# the number of new keystrokes added
+# the number of new keystrokes added since the last time it ran.
 # 
 # Here is the mac keylogger: https://code.google.com/p/logkext/wiki/ReadMe
 # (it must be configured to leave an unencrypted log file)
 
-# todo: set up a cron to post number of key strokes to featbeat
 # crontab -e
-# */10 * * * * date >> ~/temp.txt
+# every 10 minutes:
+# */10 * * * * /Users/adam/Projects/featbeat/script/keystroke_summary.rb >> ~/key-upload-log.txt
+#
+# every hour:
+# 0 * * * * /Users/adam/Projects/featbeat/script/keystroke_summary.rb >> ~/key-upload-log.txt
 
 # todo: Handle the case when the log file not present or smaller than previous lines read
 
 keylog_file = '/Library/Preferences/com.fsb.logKext'
-progress_file = '~/key-progress.txt'
+progress_file = '/Users/adam/key-progress.txt'
 
 last_read_line = `cat #{progress_file}`.to_i
 total_lines = `wc -l /Library/Preferences/com.fsb.logKext`.strip.split(' ').first.to_i
@@ -33,12 +36,18 @@ if lines_to_read > 0
 
   # process the non-character 'modifier' keys
   # TODO: rollup
-  # m = s.scan(/\<[a-z]+\>/)
+  non_characters = clean.scan(/\<[a-z]+\>/)
 
   clean = clean.gsub(/\<[a-z]+\>/i, '')
   clean = clean.gsub(/\n/i, '')
 
+  total_keystrokes = non_characters.length + clean.length
+
   puts "total keystrokes: #{clean.length}"
+
+  if total_keystrokes > 0 
+    `curl -d "from=adamloving@gmail.com&subject=#{total_keystrokes} keystrokes keystrokes typed" http://featbeat.adamloving.com/api/inbound_email`
+  end
 else
   puts "up to date"
 end
